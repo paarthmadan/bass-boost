a6fdb9ce0d799a766b7764106b481a2e230f56ac
paarthmadan <madanhockey@hotmail.com>
2017-07-19 00:35:53 -0400

    rudimentary implementation of bass boosting
    uses low-pass filter to isolate for bassline, bumps the bass signal 2db upwards, and drops the original melody 1db down

--- index.py
+++ index.py
@@ -1,5 +1,11 @@
 from pydub import AudioSegment
 from os import listdir
 
-for filename in listdir("songs"):
-    print(filename)
+song_dir = "songs"
+
+for filename in listdir(song_dir):
+    sample = AudioSegment.from_mp3(song_dir + "/" + filename)
+    filtered = sample.low_pass_filter(100)
+
+    combined = (sample - 1).overlay(filtered + 2)
+    combined.export("exports/" + filename.replace(".mp3", "") + "-export.mp3", format="mp3")
