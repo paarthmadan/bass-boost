d90379cc1d91be247b63f9e5b167c8a7129997e4
paarthmadan <madanhockey@hotmail.com>
2017-07-19 01:27:10 -0400

    calculated bass factor, using values from sin function

--- index.py
+++ index.py
@@ -1,11 +1,10 @@
 from pydub import AudioSegment
 from os import listdir
 import numpy as np
-import matplotlib.pyplot as plt
-
+import math
 
 song_dir = "songs"
-attenuate_db = 1
+attenuate_db = 0
 accentuate_db = 2
 
 
@@ -16,29 +15,17 @@ def bass_line_freq(track):
     est_mean = np.mean(sample_track)
 
     # a-value
-    est_std = 3 * np.std(sample_track) / (2 ** 0.5)
+    est_std = 3 * np.std(sample_track) / (math.sqrt(2))
 
-    # d-value
-    est_phase = 0
+    bass_factor = int(round((est_std - est_mean) * 0.005))
 
-    # k-value 4 radians
-    t = np.linspace(0, 8 * np.pi, len(sample_track))
-
-    # f(x) = a[sin(kx + t)] + c
-    estimate_function = est_std * np.sin(t + est_phase) + est_mean
-
-    plt.plot(estimate_function)
-    plt.show()
-    
-    return
+    return bass_factor
 
 for filename in listdir(song_dir):
     sample = AudioSegment.from_mp3(song_dir + "/" + filename)
-    filtered = sample.low_pass_filter(100)
-
-    bass_line_freq(sample.get_array_of_samples())
+    filtered = sample.low_pass_filter(bass_line_freq(sample.get_array_of_samples()))
 
     combined = (sample - attenuate_db).overlay(filtered + accentuate_db)
-    # combined.export("exports/" + filename.replace(".mp3", "") + "-export.mp3", format="mp3")
+    combined.export("exports/" + filename.replace(".mp3", "") + "-export.mp3", format="mp3")
 
 
